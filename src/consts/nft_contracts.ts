d8e40268ee4c13006461bc3d589f409185991cb5
My EVM <yo@myevm.org>
2025-02-02 20:48:47 +0000

    .

--- src/consts/nft_contracts.ts
+++ src/consts/nft_contracts.ts
@@ -18,9 +18,29 @@ export const NFT_CONTRACTS: NftContract[] = [
   {
     address: "0xb47a26df28923bc826100256e7d5fa01cd0817a7",
     chain: abstract,
-    title: "derplets",
-    thumbnailUrl: "/derplets.webp",  // Corrected path for public folder usage
+    title: "Derplets",
+    thumbnailUrl: "/nftcollections/derplets.webp",  // Corrected path for public folder usage
     type: "ERC721",
   },
-
+  {
+    address: "0xa6c46c07f7f1966d772e29049175ebba26262513",
+    chain: abstract,
+    title: "Pengztracted",
+    thumbnailUrl: "/nftcollections/pengztracted.png",  // Corrected path for public folder usage
+    type: "ERC721",
+  },
+  {
+    address: "0x265f307c1bdbb6f5d4B174d47F363EF5837bE98b",
+    chain: abstract,
+    title: "Abstract HotDogs",
+    thumbnailUrl: "/nftcollections/abstracthotdogs.png",  // Corrected path for public folder usage
+    type: "ERC721",
+  },
+  {
+    address: "0x516DC288e26b34557f68EA1c1Ff13576Eff8a168",
+    chain: abstract,
+    title: "Bearish",
+    thumbnailUrl: "/nftcollections/bearish.png",  // Corrected path for public folder usage
+    type: "ERC721",
+  },
 ];
